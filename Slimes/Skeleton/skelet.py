880afc01033b9cd9ba41d3c4d2f842f49fc6c408
Nadzyoki <atulupev1213@gmail.com>
2022-02-17 19:14:56 +0300

    Work on mind of slime

--- Slimes/Skeleton/skelet.py
+++ Slimes/Skeleton/skelet.py
@@ -7,18 +7,25 @@ class Slime(Entity):
         self.hp = 100
         self.dist = 20
         self.speed = 5
-        self.knowledge = {
+        self.knowledge_dis = {
             "home" : 0,
-
-
+            "fire" : 0,
+            "water": 0,
+            "rock" : 0,
+            "tree" : 0,
+            "metall":0
         }
 
+    def update(self):
+        pass
+
+#get data other around
     def Spray(self):
         for i in range(360):
             self.rotation_directions = i
             v = raycast(origin=self, direction=self.forward, ignore=(self,), distance=self.dist)
             if v.hit:
-                match v.entity.tag :
+                match v.entity.tag:
                     case "friend":
                         v.entity.Know(self.MyKnow())
                     case "warrior":
@@ -28,9 +35,12 @@ class Slime(Entity):
                     case _:
                         print("ERROR WTF THIS IS???????")
 
-
     def MyKnow(self):
-        pass
+        ret = []
+        ret[0] = random.choice(self.knowledge_dis.keys())
+        ret[1] = self.knowledge_dis[ret[0]]
+        return ret
 
-    def Know(self):
-        pass
+    def Know(self,kn):
+        if kn[1] < self.knowledge_dis[kn[0]]:
+            self.knowledge_dis[kn[0]] = kn[1]
